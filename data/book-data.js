9f081ae634ab1d373649df0603dc3d217b75efb4
yen0918 <79597502+yen0918@users.noreply.github.com>
2023-06-03 19:58:55 +0800

    initial commit

---
+++ data/book-data.js
@@ -0,0 +1,603 @@
+var bookData = [{
+    "BookId":1,
+    "BookCategory":"資料庫",
+    "BookName":"The Catcher in the Rye",
+    "BookAuthor":"J.D. Salinger",
+    "BookBoughtDate":"1990-06-18",
+    "BookPublisher":"創創公司"
+},
+{
+    "BookId":2,
+    "BookCategory":"資料庫",
+    "BookName":"To Kill a Mockingbird",
+    "BookAuthor":"Harper Lee",
+    "BookBoughtDate":"1990-06-18",
+    "BookPublisher":"零壹"
+},
+{
+    "BookId":3,
+    "BookCategory":"資料庫",
+    "BookName":"1984",
+    "BookAuthor":"George Orwell",
+    "BookBoughtDate":"1990-06-18",
+    "BookPublisher":"WESLEY"
+},
+{
+    "BookId":4,
+    "BookCategory":"資料庫",
+    "BookName":"Pride and Prejudice",
+    "BookAuthor":"Jane Austen",
+    "BookBoughtDate":"1990-06-18",
+    "BookPublisher":"全華"
+},
+{
+    "BookId":5,
+    "BookCategory":"資料庫",
+    "BookName":"The Great Gatsby",
+    "BookAuthor":"F. Scott Fitzgerald",
+    "BookBoughtDate":"1990-06-18",
+    "BookPublisher":"學貫行銷股份有限公司"
+}
+,
+{
+    "BookId":6,
+    "BookCategory":"資料庫",
+    "BookName":"Moby Dick",
+    "BookAuthor":"Herman Melville",
+    "BookBoughtDate":"1990-06-18",
+    "BookPublisher":"學貫行銷股份有限公司"
+}
+,
+{
+    "BookId":7,
+    "BookCategory":"資料庫",
+    "BookName":"Jane Eyre",
+    "BookAuthor":"Charlotte Bronte",
+    "BookBoughtDate":"1990-06-18",
+    "BookPublisher":"Mc Graw Hill"
+}
+,
+{
+    "BookId":8,
+    "BookCategory":"資料庫",
+    "BookName":"The Lord of the Rings",
+    "BookAuthor":"J.R.R. Tolkien",
+    "BookBoughtDate":"1990-06-18",
+    "BookPublisher":"創創公司"
+},
+{
+    "BookId":9,
+    "BookCategory":"資料庫",
+    "BookName":"The Chronicles of Narnia",
+    "BookAuthor":"C.S. Lewis",
+    "BookBoughtDate":"1990-06-18",
+    "BookPublisher":"維科"
+},
+{
+    "BookId":10,
+    "BookCategory":"資料庫",
+    "BookName":"Harry Potter and the Philosopher's Stone",
+    "BookAuthor":"J.K. Rowling",
+    "BookBoughtDate":"1990-06-18",
+    "BookPublisher":"創創公司"
+},
+{
+    "BookId":11,
+    "BookCategory":"資料庫",
+    "BookName":"The Hobbit" ,
+    "BookAuthor":"J.R.R. Tolkien",
+    "BookBoughtDate":"1990-06-18",
+    "BookPublisher":"維科"
+},
+{
+    "BookId":12,
+    "BookCategory":"資料庫",
+    "BookName":"The Grapes of Wrath",
+    "BookAuthor":" John Steinbeck",
+    "BookBoughtDate":"1990-06-18",
+    "BookPublisher":"大秦出版社"
+},
+{
+    "BookId":13,
+    "BookCategory":"網際網路",
+    "BookName":"The Odyssey",
+    "BookAuthor":"Homer",
+    "BookBoughtDate":"1990-06-18",
+    "BookPublisher":"經濟部中小企業"
+}
+,
+{
+    "BookId":14,
+    "BookCategory":"網際網路",
+    "BookName":"Brave New World",
+    "BookAuthor":"Aldous Huxley",
+    "BookBoughtDate":"1990-06-18",
+    "BookPublisher":"碁峰"
+}
+,
+{
+    "BookId":15,
+    "BookCategory":"網際網路",
+    "BookName":"The Divine Comedy",
+    "BookAuthor":"Dante Alighieri",
+    "BookBoughtDate":"1990-06-18",
+    "BookPublisher":"Addison Wesley"
+}
+,
+{
+    "BookId":16,
+    "BookCategory":"網際網路",
+    "BookName":"The Adventures of Huckleberry Finn",
+    "BookAuthor":"Mark Twain",
+    "BookBoughtDate":"1990-06-18",
+    "BookPublisher":"Addison Wesley"
+},
+{
+    "BookId":17,
+    "BookCategory":"網際網路",
+    "BookName":"Don Quixote",
+    "BookAuthor":"Miguel de Cervantes Saavedra",
+    "BookBoughtDate":"1990-06-18",
+    "BookPublisher":"O'Reilly"
+},
+{
+    "BookId":18,
+    "BookCategory":"網際網路",
+    "BookName":"The Scarlet Letter",
+    "BookAuthor":"Nathaniel Hawthorne",
+    "BookBoughtDate":"1990-06-18",
+    "BookPublisher":"商周"
+},
+{
+    "BookId":19,
+    "BookCategory":"網際網路",
+    "BookName":"The Picture of Dorian Gray",
+    "BookAuthor":"Oscar Wilde",
+    "BookBoughtDate":"1990-06-18",
+    "BookPublisher":"全華圖書"
+},
+{
+    "BookId":20,
+    "BookCategory":"網際網路",
+    "BookName":"War and Peace",
+    "BookAuthor":"Leo Tolstoy",
+    "BookBoughtDate":"1990-06-18",
+    "BookPublisher":"巨擘"
+},
+{
+    "BookId":21,
+    "BookCategory":"網際網路",
+    "BookName":"CARRIER GRADE VOICE OVER IP",
+    "BookAuthor":"DANIEL COLLINS",
+    "BookBoughtDate":"1990-06-18",
+    "BookPublisher":"和碩"
+}
+,
+{
+    "BookId":22,
+    "BookCategory":"網際網路",
+    "BookName":"TCP/IP SOCKETS IN C#",
+    "BookAuthor":"David D. Makofske",
+    "BookBoughtDate":"1990-06-18",
+    "BookPublisher":"博碩"
+}
+,
+{
+    "BookId":23,
+    "BookCategory":"網際網路",
+    "BookName":"Implementing Virtual Private Networks",
+    "BookAuthor":"Steven Brown",
+    "BookBoughtDate":"1990-06-18",
+    "BookPublisher":"工研院"
+}
+,
+{
+    "BookId":24,
+    "BookCategory":"網際網路",
+    "BookName":"C#.NET網際網路程式設計",
+    "BookAuthor":"黃嘉輝",
+    "BookBoughtDate":"1990-06-18",
+    "BookPublisher":"創創公司"
+},
+{
+    "BookId":25,
+    "BookCategory":"應用系統整合",
+    "BookName":"捷運工程之監控及數據採集系統設計對策",
+    "BookAuthor":"祝懷吉.林明正",
+    "BookBoughtDate":"1990-06-18",
+    "BookPublisher":"工研院"
+},
+{
+    "BookId":26,
+    "BookCategory":"應用系統整合",
+    "BookName":"Crystal Reports 10整合實戰應用",
+    "BookAuthor":"鄒光宗",
+    "BookBoughtDate":"1990-06-18",
+    "BookPublisher":"工研院"
+},
+{
+    "BookId":27,
+    "BookCategory":"家庭保健",
+    "BookName":"兒童讀經教育",
+    "BookAuthor":"王財貴",
+    "BookBoughtDate":"1990-06-18",
+    "BookPublisher":"軟協"
+},
+{
+    "BookId":28,
+    "BookCategory":"家庭保健",
+    "BookName":"灼燙傷手冊",
+    "BookAuthor":"馬偕醫院",
+    "BookBoughtDate":"1990-06-18",
+    "BookPublisher":"軟協"
+},
+{
+    "BookId":29,
+    "BookCategory":"家庭保健",
+    "BookName":"如何保健自己",
+    "BookAuthor":"鐘文出版社",
+    "BookBoughtDate":"1990-06-18",
+    "BookPublisher":"軟協"
+}
+,
+{
+    "BookId":30,
+    "BookCategory":"家庭保健",
+    "BookName":"怎樣保護自己",
+    "BookAuthor":"張青史",
+    "BookBoughtDate":"1990-06-18",
+    "BookPublisher":"PTR/PH"
+}
+,
+{
+    "BookId":31,
+    "BookCategory":"家庭保健",
+    "BookName":"您的家庭醫師--兒童篇",
+    "BookAuthor":"瓦爾曼",
+    "BookBoughtDate":"1990-06-18",
+    "BookPublisher":"創創公司"
+}
+,
+{
+    "BookId":32,
+    "BookCategory":"家庭保健",
+    "BookName":"您的家庭醫師-女性篇",
+    "BookAuthor":"瓦爾曼",
+    "BookBoughtDate":"1990-06-18",
+    "BookPublisher":"PTR/PH"
+},
+{
+    "BookId":33,
+    "BookCategory":"家庭保健",
+    "BookName":"您的家庭醫師-男性篇",
+    "BookAuthor":"瓦爾曼",
+    "BookBoughtDate":"1990-06-18",
+    "BookPublisher":"PTR/PH"
+},
+{
+    "BookId":34,
+    "BookCategory":"家庭保健",
+    "BookName":"不要小看兒童疼痛",
+    "BookAuthor":"瓦爾曼",
+    "BookBoughtDate":"1990-06-18",
+    "BookPublisher":"經濟部中小企業"
+},
+{
+    "BookId":35,
+    "BookCategory":"家庭保健",
+    "BookName":"認識成人預防保健",
+    "BookAuthor":"中央健康保險局",
+    "BookBoughtDate":"1990-06-18",
+    "BookPublisher":"天瓏"
+},
+{
+    "BookId":36,
+    "BookCategory":"家庭保健",
+    "BookName":"健康小百科",
+    "BookAuthor":"梅約醫學中心",
+    "BookBoughtDate":"1990-06-18",
+    "BookPublisher":"博碩"
+},
+{
+    "BookId":37,
+    "BookCategory":"語言",
+    "BookName":"聖殿祭司的ASP .NET 2.0專家技術手冊-使用C#-2",
+    "BookAuthor":"奚江華",
+    "BookBoughtDate":"1990-06-18",
+    "BookPublisher":"桂冠圖書"
+}
+,
+{
+    "BookId":38,
+    "BookCategory":"語言",
+    "BookName":"UML系統分析與設計",
+    "BookAuthor":"游峰碩",
+    "BookBoughtDate":"1990-06-18",
+    "BookPublisher":"金融中心"
+}
+,
+{
+    "BookId":39,
+    "BookCategory":"語言",
+    "BookName":"Enterprise Java Programming with IBM WebSphere",
+    "BookAuthor":"Brown, Graig.....",
+    "BookBoughtDate":"1990-06-18",
+    "BookPublisher":"Microsoft"
+}
+,
+{
+    "BookId":40,
+    "BookCategory":"語言",
+    "BookName":"Visual Basic 6.0資料庫程式設計",
+    "BookAuthor":"王國榮",
+    "BookBoughtDate":"1990-06-18",
+    "BookPublisher":"內政部"
+},
+{
+    "BookId":41,
+    "BookCategory":"語言",
+    "BookName":"實用JAVA樣式",
+    "BookAuthor":"彭仁松譯",
+    "BookBoughtDate":"1990-06-18",
+    "BookPublisher":"東華"
+},
+{
+    "BookId":42,
+    "BookCategory":"語言",
+    "BookName":"深入淺出軟體開發",
+    "BookAuthor":"楊仁和編譯",
+    "BookBoughtDate":"1990-06-18",
+    "BookPublisher":"創創公司"
+},
+{
+    "BookId":43,
+    "BookCategory":"語言",
+    "BookName":"深入淺出C#",
+    "BookAuthor":"楊仁和編譯",
+    "BookBoughtDate":"1990-06-18",
+    "BookPublisher":"GSS"
+},
+{
+    "BookId":44,
+    "BookCategory":"語言",
+    "BookName":"Flex +Air 程式開發",
+    "BookAuthor":"宋志峰著",
+    "BookBoughtDate":"1990-06-18",
+    "BookPublisher":"McGraw-Hill"
+},
+{
+    "BookId":45,
+    "BookCategory":"語言",
+    "BookName":"SQL Server 2008 SSIS整合服務",
+    "BookAuthor":"胡百敬，姚巧玫著",
+    "BookBoughtDate":"1990-06-18",
+    "BookPublisher":"McGraw-Hill"
+}
+,
+{
+    "BookId":46,
+    "BookCategory":"語言",
+    "BookName":"重構-改善既有程式的設計 第二版",
+    "BookAuthor":"侯捷/熊節譯",
+    "BookBoughtDate":"1990-06-18",
+    "BookPublisher":"創創公司"
+}
+,
+{
+    "BookId":47,
+    "BookCategory":"語言",
+    "BookName":"物件導向-系統分析設計與實作",
+    "BookAuthor":"東海林",
+    "BookBoughtDate":"1990-06-18",
+    "BookPublisher":"Meghan-Kiffer Press"
+}
+,
+{
+    "BookId":48,
+    "BookCategory":"語言",
+    "BookName":"Java 2全方位學習  J2SE 5.0增修版",
+    "BookAuthor":"朱仲傑",
+    "BookBoughtDate":"1990-06-18",
+    "BookPublisher":"QUE E&T"
+},
+{
+    "BookId":49,
+    "BookCategory":"管理",
+    "BookName":"專案經理的教戰守則-成功PM的18項修練#1",
+    "BookAuthor":"熊培霖",
+    "BookBoughtDate":"1990-06-18",
+    "BookPublisher":"MC GRAW HILL"
+},
+{
+    "BookId":50,
+    "BookCategory":"管理",
+    "BookName":"企管大師報到---創造管理的五十位思想家",
+    "BookAuthor":"科雷納",
+    "BookBoughtDate":"1990-06-18",
+    "BookPublisher":"創創公司"
+},
+{
+    "BookId":51,
+    "BookCategory":"管理",
+    "BookName":"我懂了,專案管理",
+    "BookAuthor":"James P. Lewis",
+    "BookBoughtDate":"1990-06-18",
+    "BookPublisher":"長諾"
+},
+{
+    "BookId":52,
+    "BookCategory":"管理",
+    "BookName":"改變企業經營體質--訓練與發展",
+    "BookAuthor":"李長貴",
+    "BookBoughtDate":"1990-06-18",
+    "BookPublisher":"碁峰"
+},
+{
+    "BookId":53,
+    "BookCategory":"管理",
+    "BookName":"2010創業勝經 創業楷模奮鬥故事",
+    "BookAuthor":"中國青年創業協會總會",
+    "BookBoughtDate":"1990-06-18",
+    "BookPublisher":"Addison Wesley"
+}
+,
+{
+    "BookId":54,
+    "BookCategory":"管理",
+    "BookName":"Inside Intel--英特爾三十年風雲",
+    "BookAuthor":"堤姆.傑克遜",
+    "BookBoughtDate":"1990-06-18",
+    "BookPublisher":"創創公司"
+}
+,
+{
+    "BookId":55,
+    "BookCategory":"管理",
+    "BookName":"善用事業夥伴--電話",
+    "BookAuthor":"江淑芬",
+    "BookBoughtDate":"1990-06-18",
+    "BookPublisher":"松格"
+}
+,
+{
+    "BookId":56,
+    "BookCategory":"管理",
+    "BookName":"Mastering the Unpredictable#1",
+    "BookAuthor":"Keith D.Swenson",
+    "BookBoughtDate":"1990-06-18",
+    "BookPublisher":"巨擘"
+},
+{
+    "BookId":57,
+    "BookCategory":"管理",
+    "BookName":"生產管理-上",
+    "BookAuthor":"趙山",
+    "BookBoughtDate":"1990-06-18",
+    "BookPublisher":"創創公司"
+},
+{
+    "BookId":58,
+    "BookCategory":"管理",
+    "BookName":"The Power of VISION",
+    "BookAuthor":"Joel Arthur Barker",
+    "BookBoughtDate":"1990-06-18",
+    "BookPublisher":"GSS"
+},
+{
+    "BookId":59,
+    "BookCategory":"管理",
+    "BookName":"一對一經理人(e世代成功的顧客關係管理)",
+    "BookAuthor":"唐.派普/瑪莎.羅傑斯",
+    "BookBoughtDate":"1990-06-18",
+    "BookPublisher":"創創公司"
+},
+{
+    "BookId":60,
+    "BookCategory":"管理",
+    "BookName":"企業績效管理",
+    "BookAuthor":"GSS",
+    "BookBoughtDate":"1990-06-18",
+    "BookPublisher":"GSS"
+},
+{
+    "BookId":61,
+    "BookCategory":"行銷",
+    "BookName":"Strategic and  Competitive Analysis",
+    "BookAuthor":"Craig S Fleisher, Babette E Be",
+    "BookBoughtDate":"1990-06-18",
+    "BookPublisher":"創創公司"
+}
+,
+{
+    "BookId":62,
+    "BookCategory":"行銷",
+    "BookName":"綠色企業",
+    "BookAuthor":"John Davis",
+    "BookBoughtDate":"1990-06-18",
+    "BookPublisher":"三民書局"
+}
+,
+{
+    "BookId":63,
+    "BookCategory":"行銷",
+    "BookName":"THE MISSION STATEMENT BOOK",
+    "BookAuthor":"Jeffrey Abrahams",
+    "BookBoughtDate":"1990-06-18",
+    "BookPublisher":"讀品文化"
+}
+,
+{
+    "BookId":64,
+    "BookCategory":"行銷",
+    "BookName":"BUSINESS PROPOSALS",
+    "BookAuthor":"TOM SANT",
+    "BookBoughtDate":"1990-06-18",
+    "BookPublisher":"Addison_Wesley"
+},
+{
+    "BookId":66,
+    "BookCategory":"行銷",
+    "BookName":"科技頑童沃茲尼克",
+    "BookAuthor":"沃茲尼克,史密斯",
+    "BookBoughtDate":"1990-06-18",
+    "BookPublisher":"全球智慧文化"
+},
+{
+    "BookId":65,
+    "BookCategory":"行銷",
+    "BookName":"天下1000大企業光碟片--2005旗艦版光碟片",
+    "BookAuthor":"天下雜誌",
+    "BookBoughtDate":"1990-06-18",
+    "BookPublisher":"聯輔中心"
+},
+{
+    "BookId":67,
+    "BookCategory":"行銷",
+    "BookName":"這樣想，就對了",
+    "BookAuthor":"林有田",
+    "BookBoughtDate":"1990-06-18",
+    "BookPublisher":"憲業企管"
+},
+{
+    "BookId":68,
+    "BookCategory":"行銷",
+    "BookName":"無體行銷",
+    "BookAuthor":"Beckwith",
+    "BookBoughtDate":"1990-06-18",
+    "BookPublisher":"聯輔中心"
+},
+{
+    "BookId":69,
+    "BookCategory":"行銷",
+    "BookName":"成交--贏得顧客心的對談式銷售法",
+    "BookAuthor":"琳達里查察森",
+    "BookBoughtDate":"1990-06-18",
+    "BookPublisher":"聯輔中心"
+}
+,
+{
+    "BookId":71,
+    "BookCategory":"行銷",
+    "BookName":"雲端策略",
+    "BookAuthor":"陳瀅等著",
+    "BookBoughtDate":"1990-06-18",
+    "BookPublisher":"經濟部"
+}
+,
+{
+    "BookId":70,
+    "BookCategory":"行銷",
+    "BookName":"雲端運算投資攻略",
+    "BookAuthor":"Steve Waterhouse",
+    "BookBoughtDate":"1990-06-18",
+    "BookPublisher":"中國生產力"
+}
+,
+{
+    "BookId":72,
+    "BookCategory":"行銷",
+    "BookName":"團隊銷售無往不利",
+    "BookAuthor":"Steve Waterhouse",
+    "BookBoughtDate":"1990-06-18",
+    "BookPublisher":"華泰"
+}]
